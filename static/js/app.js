2d7b1081eba76de0d17147b3dd675334f4b3399c
Caleb Schwarze <ces55739@bethel.edu>
2020-10-13 13:37:40 -0500

    added in the logic to properly click calculator buttons

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,22 @@ $( document ).ready(function() {
     calc_btn = $(".calc-btn")
     calc_btn.click(function() {
         // todo: need to add logic so you can't put multiple equations
-        full_equation = $('#full-equation');
-        full_equation.val(full_equation.val() + $(this).val());
+        full_equation = $('#full-equation').val();
+        new_input = $(this).val();
+
+        // you must start with a number, otherwise the equation doesn't make sense.
+        if(full_equation == '' && !$.isNumeric(new_input) ) {
+            return;
+        }
+
+        // if a new operator is entered and the last was an operator, replace with new operator
+        // todo: this could be improved by using regex.
+        if(!$.isNumeric(new_input) && ['+', '-', '*', '/'].includes(full_equation.charAt(full_equation.length - 2))){
+            full_equation = full_equation.slice(0, -3);
+        }
+
+        // add value to the full equation
+        $('#full-equation').val(full_equation + new_input);
     })
 
 
